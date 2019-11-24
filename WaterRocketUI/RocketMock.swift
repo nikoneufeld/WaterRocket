ec8d19ff2bfaaf0d2338b2376c31abb50f393785
jakob <jakob.n.neufeld@gmail.com>
2019-11-24 15:28:33 +0100

    Working mock-up simulates flight.

--- WaterRocketUI/RocketMock.swift
+++ WaterRocketUI/RocketMock.swift
@@ -8,23 +8,54 @@
 
 import Foundation
 import Combine
-/// A  mock for our model with out butooth, so th ui designer can see our model!
+import Dispatch
+
+/// A  mock for our model with out bluetooth, so th ui designer can see our model!
 class RocketMock: Rocket {
-    var objectWillChange: PassthroughSubject<Void, Never>
+    init() {
+        altitude = 0.0
+        maxAltitude = 0.0
+    }
+    var objectWillChange = PassthroughSubject<Void, Never>()
     
-    var altituide: Double
+    @Published var altitude: Double
     
-    var delegate: RocketDelegate?
+    @Published var delegate: RocketDelegate?
     
-    var maxAltitude: Double
+    @Published var maxAltitude: Double
     
     func reset() {
-        objectWillChange.send()
+        altitude = 0.0
+        maxAltitude = 0.0
     }
     
     func clearMaxAlt() {
-        <#code#>
+        maxAltitude = 0.0
+    }
+    // simulator specific methods & properties
+        
+    // simulate a flight of duration with timeResolution in seconds
+    func fly(duration: Int, timeResolution: Double = 0.1) {
+        let queue = DispatchQueue.global()
+        let work = DispatchWorkItem(block: {
+            var t = 0.0
+            self.altitude = 0.0
+            while (t < Double(duration) / 2.0) {
+                self.altitude += 9.81 * t * t / 2.0
+                usleep(useconds_t(1000000.0 * timeResolution))
+                t += timeResolution
+                if self.maxAltitude < self.maxAltitude {
+                    self.maxAltitude = self.altitude
+                }
+            }
+            while (t < Double(duration)) {
+                self.altitude -= 9.81 * t * t / 2.0
+                usleep(useconds_t(1000000.0 * timeResolution))
+            }
+            self.altitude = 0.0
+        })
+        queue.async(execute: work)
     }
     
-    
 }
+
